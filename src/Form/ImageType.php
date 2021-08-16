44b5e3f0df1055a4aec8d57ba9d52b601ac06f99
jfsenechal <jfsenechal@gmail.com>
2021-08-16 09:51:52 +0200

    new header

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -6,21 +6,11 @@ use AcMarche\Patrimoine\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add(
-                'file',
-                DropzoneType::class,
-                [
-                    'label' => 'Fichier',
-                    'multiple' => true,
-                ]
-            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
