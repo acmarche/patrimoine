c2e52e5769b21476df838536385680745af90ace
jfsenechal <jfsenechal@gmail.com>
2024-02-06 10:33:42 +0100

    up sf 7.0

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace AcMarche\Patrimoine\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_PATRIMOINE_ADMIN')]
